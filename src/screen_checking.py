76fb0b7b24dcc0c7ec8fcd87a31b62f5e6b04e95
Ed <skinnyed@gmail.com>
2019-04-26 19:05:51 +0100

    Extra bad pin test

--- src/screen_checking.py
+++ src/screen_checking.py
@@ -220,9 +220,9 @@ class CheckingScreen(Screen):
             
             p = 1
             while p < len(self.data_set[0]):
-                self.rpi_input_pin = int(self.data_set[0][p])
                 if sys.platform != "win32":
                     try:
+                        self.rpi_input_pin = int(self.data_set[0][p])
                         GPIO.setup(self.rpi_input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # set a port/pin as an output   
                     except:
                         self.error_reason = "Logic matrix: Unable to assign RasPi pin " + str(self.rpi_input_pin) + " as input."
